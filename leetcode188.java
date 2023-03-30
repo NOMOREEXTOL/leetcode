07ab2ff971f474652bf2cc016ba06c4dd1e7861a
NOMOREEXTOL <128805159+NOMOREEXTOL@users.noreply.github.com>
2023-03-30 20:51:28 +0800

    Add files via upload

---
+++ leetcode188.java
@@ -0,0 +1,27 @@
+/**
+ * 实际运行2ms,39.3MB
+ */
+
+
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        int dp[] = new int[2 * k + 1];
+        for (int i = 1; i <= 2 * k; i++) {
+            if (i % 2 == 1) {
+            dp[i] = -prices[0];
+            }
+        }
+        
+        for (int j = 1 ; j < prices.length ; ++j) {
+            for (int m = 1 ; m <= 2 * k ; m++) {
+                if (m % 2 == 1) {
+                    dp[m] = Math.max(dp[m],dp[m-1] - prices[j]);
+                } else {
+                    dp[m] = Math.max(dp[m],dp[m-1] + prices[j]);
+                }
+            }
+
+        }
+        return dp[2 * k];
+    }
+}
