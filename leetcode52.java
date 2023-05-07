0a359ad0d8ab8d528a1abcc7dafc83c67b7d5e0d
NOMOREEXTOL <128805159+NOMOREEXTOL@users.noreply.github.com>
2023-05-07 11:02:31 +0800

    Add files via upload

---
+++ leetcode52.java
@@ -0,0 +1,32 @@
+/**
+ *  5ms,42.5MB(45.52,5.02 % )
+ */
+
+
+
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+
+
+        // 初始化
+        for (int i = 0; i <= word1.length() ; i++) {
+            dp[i][0] = i;
+        }
+
+        for (int j = 0; j <= word2.length(); j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= word1.length(); i++) {
+            for (int j = 1; j <= word2.length(); j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i-1][j] + 1,Math.min(dp[i][j-1] + 1,dp[i-1][j-1] + 1));
+                }
+            }
+        }
+        return dp[word1.length()][word2.length()];
+    }
+}
